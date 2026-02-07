1df5b6ec79ba9bf93bee4b7dd00eaebb9f2dfda2
Kiires <113013297+Kiires@users.noreply.github.com>
2026-02-07 18:37:25 +0300

    upd

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
 
     if (username === 'user' && password === 'password') {
-        window.location.href = '../MainPages/fondBook.html';
+        window.location.href = '/itmo-web-y26/MainPages/fondBook.html';
     } else {
         alert('Неверный логин или пароль');
     }
